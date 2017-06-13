e6542ec0dc82cce2d311c21f7816620497a26a5c
Danny Garcia <danny_garcia@apple.com>
2017-06-13 01:48:21 -0700

    simple build

--- _site/static/js/colorcycle.js
+++ _site/static/js/colorcycle.js
@@ -1,16 +1,16 @@
 // prep vertically-split text
-document.querySelectorAll('main footer p').forEach(s => {
-	const splitNode = s.querySelector('.split');
-	let html = '';
-	splitNode.innerText.split('').forEach(n => {
-		if (typeof n !== 'undefined') {
-			html += '<span class=\"shiny\">' + n + '</span>';
-		}
-	});
-	splitNode.innerHTML = html;
-});
+// document.querySelectorAll('main footer p').forEach(s => {
+// 	const splitNode = s.querySelector('.split');
+// 	let html = '';
+// 	splitNode.innerText.split('').forEach(n => {
+// 		if (typeof n !== 'undefined') {
+// 			html += '<span class=\"shiny\">' + n + '</span>';
+// 		}
+// 	});
+// 	splitNode.innerHTML = html;
+// });
 
-const shinies = document.querySelectorAll('.shiny, article a');
+const shinies = document.querySelectorAll('a');
 shinies.forEach(s => {
 	s.cachedTop = s.getBoundingClientRect().top;
 	s.rgb = getComputedStyle(s)
@@ -26,17 +26,19 @@ cycleColor = (s, scroll, height) => {
 	s.style.color = 'hsl('+(s.hsl[0] + (Math.sin((scroll - s.cachedTop) / (height / 5)) * 10))+','+(s.hsl[1] + (Math.sin((scroll - s.cachedTop) / (height / 5)) * 40))+'%,'+(s.hsl[2] + (Math.sin((scroll - s.cachedTop) / (height / 5)) * 40))+'%)';
 };
 
-cycleColors = () => {
-	shinies.forEach(s => cycleColor(s, window.scrollY, window.innerHeight));
+cycleColors = (influence) => {
+	shinies.forEach(s => cycleColor(s, window.scrollY + influence, window.innerHeight));
 };
 
-let y = scrollY;
-// requestAnimationFrame(raf = () => {
-// 	if (y !== scrollY) {
-// 		y = scrollY;
-// 		cycleColors();
-// 		raf();
-// 	}
-// });
-
+// let y = scrollY;
+let initY = -window.innerHeight/2;
+const raf = () => {
+	if (initY < 0) {
+		initY++;
+	}
+	// y = scrollY;
+	cycleColors(initY);
+	requestAnimationFrame(raf);
+}
+raf();
 // addEventListener('scroll', raf);
