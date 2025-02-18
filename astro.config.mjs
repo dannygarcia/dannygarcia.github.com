531b131ef8ae2883116fa3a28f7007dd8fed920b
Danny Garcia <danny.d.g@gmail.com>
2025-02-17 23:48:17 -0800

    astro

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  output: 'static',
+  adapter: netlify(),
+  vite: {
+    ssr: {
+      noExternal: ['three']
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: './src/client/main.ts'
+        }
+      }
+    }
+  },
+  integrations: []
+});
