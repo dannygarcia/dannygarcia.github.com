cedb83f8aa130dc3a517e722dd784ad11da54e4d
Danny Garcia <danny.d.g@gmail.com>
2026-05-08 21:47:13 -0700

    Add publish flag to filter blog posts in build

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
+const DEFAULT_IS_PUBLISHED = true;
+
 export const collections = {
   blog: defineCollection({
     schema: z.object({
       title: z.string(),
       publishDate: z.date(),
       description: z.string(),
+      isPublished: z.boolean().default(DEFAULT_IS_PUBLISHED),
     })
   })
 };
